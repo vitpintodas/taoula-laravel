894c4f4a1e1761e8a83274bf9b6c430cc19c5050
Andrea Ferrer <andreaferrer361@gmail.com>
2023-06-01 13:12:47 +0200

    FEAT: Création du component Header

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -3,11 +3,13 @@
 import BaseMainCards from './base/BaseMainCards.vue';
 import BaseSettingsButton from './base/BaseSettingsButton.vue';
 import BaseSmallCards from './base/BaseSmallCards.vue';
+import TheHeader from './base/TheHeader.vue';
 
 
 </script>
 
 <template>  
+  <TheHeader/>
   <BaseMainCards title="Smash mouth - All star" description="Heure musique" type="live" link="#unLien" info="05:20:23"/>
   <BaseMainCards title="Billets pour paléo" description="Tente de ganger des billets pour paléo" type="contest" link="#unlien2"/>
   <BaseMainCards title="Smash mouth - All star" description="Heure musique" type="star" link="#unLien" info="05:20:23"/>
---
+++ resources/js/base/TheHeader.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { getImagePath } from '../utils/getImagePath';
+
+//A changer pour mettre le nombre de coins de l'utilisateur
+const nbCoins = 0;
+
+//A changer pour mettre si le bouton back est possible ou non
+const backPossible = true;
+
+
+</script>
+
+<template>
+
+<header>
+  <div class="back-arrow">
+    <img :src="getImagePath('arrow-back')" href="#shop" v-if="backPossible">
+  </div>
+
+  <div class="c3-logo">
+    <img :src="getImagePath('c3-logo')" href="#home">
+  </div>
+
+  <div class="right-section">
+    <div class="coins">
+      <img :src="getImagePath('coin')">
+      <p>{{ nbCoins }}</p>
+    </div>
+
+    <div class="shop">
+      <img :src="getImagePath('shop')" href="#shop">
+    </div>
+  </div>
+</header>
+
+
+
+
+</template>
+
+
+<style>
+    
+</style>
