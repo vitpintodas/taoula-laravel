e968b151259628c06cea3c096cd39b341db65440
owdji <omar.gonin@gmail.com>
2023-06-08 12:25:48 +0200

    FEAT: Page live

--- resources/js/components/Live.vue
+++ resources/js/components/Live.vue
@@ -2,6 +2,8 @@
 import { ref } from 'vue';
 import Interact from './Interact/Interact.vue';
 import InteractResult from './Interact/InteractResult.vue';
+import BaseAudio from '../base/BaseAudio.vue';
+import BaseSmallCards from '../base/BaseSmallCards.vue';
 
 const pollFakeOption = ref([{
     title: 'Iggy Pop the passenger',
@@ -14,6 +16,7 @@ const pollFakeOption = ref([{
     title: 'The Beatles Hey Jude',
     selected: false
 }])
+const showName = ref('...')
 // const multipleChoiceChallengeFakeOptions = ref([{
 //     title: 'Iggy Pop the passenger',
 //     selected: false,
@@ -68,6 +71,27 @@ const getResult = (theResult) => {
     //Si challenge, regarder si la réponse est correct
 }
 
+//fetch from this url https://il.srgssr.ch/integrationlayer/2.0/mediaComposition/byUrn/urn:rts:audio:3262363.json?onlyChapters=true&vector=portalplay
+const getShowName = async() => {
+    // const response = await fetch("http://example.com/movies.json");
+    // const jsonData = await response.json();
+    // console.log(jsonData);
+    // Make a request to fetch the HTML content
+    const url = 'https://il.srgssr.ch/integrationlayer/2.0/mediaComposition/byUrn/urn:rts:audio:3262363.json?onlyChapters=true&vector=portalplay';
+    const response = await fetch(url);
+    const text = await response.text();
+    const data = JSON.parse(text);
+    console.log(data.analyticsMetadata.media_show);
+
+    showName.value = data.analyticsMetadata.media_show
+}
+
+setInterval(() => {
+    getShowName()
+}, 10000);
+
+console.log('api', getShowName())
+
 //Pour le résultat on doit pouvoir accéder à ces 3 possibilités : 
 //poll : pour afficher le résultat du sondage
 //challenge : l'utilisateur sera mis au courant en cas de victoire
@@ -77,12 +101,24 @@ const getResult = (theResult) => {
 </script>
 
 <template>
-    <!-- <iframe src="https://www.rts.ch/audio-podcast/livepopup/couleur3/" width="100%" height="166" frameborder="0" allowfullscreen></iframe> -->
-    <Interact 
-    v-if="result == null" 
-    title="Quelle est la pire ville de Suisse" 
-    :options="pollFakeOption" 
-    @emit-poll="getResult($event)" 
-    type="textInput"/>
-    <InteractResult v-if="result" :interactType="'challengeLoss'" :results="fakeResults"/>
+    <div class="live-container">
+
+        <div>
+            <BaseAudio />
+            <Interact 
+            v-if="result == null" 
+            title="Vote pour la prochaine musique" 
+            :options="pollFakeOption" 
+            @emit-poll="getResult($event)" 
+            type="multipleChoice"/>
+            <InteractResult v-if="result" :interactType="'poll'" :results="fakeResults"/>
+        </div>
+
+        <div class="live-bottomInfoContainer">
+            <p class="descriptionBold">En cours</p>
+            <BaseSmallCards :title="showName" type="music" :like-status="false" />
+        </div>
+
+    </div>
+
 </template>
