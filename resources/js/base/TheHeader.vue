624bf4e97715a2f6f2829ba6416d467a35f994c0
Andrea Ferrer <andreaferrer361@gmail.com>
2023-06-08 10:08:59 +0200

    FIX: issues with header css

--- resources/js/base/TheHeader.vue
+++ resources/js/base/TheHeader.vue
@@ -1,51 +1,54 @@
 <script setup>
-
+import { ref, onMounted } from 'vue';
 import { getImagePath } from '../utils/getImagePath';
 
-//A changer pour mettre le nombre de coins de l'utilisateur
 const nbCoins = 0;
+const isBackPossible = ref(false);
 
-//Permet de retourner en arrière
 const goBack = () => {
   window.history.back();
-}
+};
 
-//Fonction permettant de voir si un retour en arrièere est possible
-const backPossible = () => {
-  if (window.history.length > 1) {
-    return true;
-  }
-}
+const updateBackPossible = () => {
+  isBackPossible.value = window.history.length > 1;
+};
 
+onMounted(() => {
+  updateBackPossible();
+});
 
+window.addEventListener('popstate', () => {
+  updateBackPossible();
+});
 </script>
 
 <template>
-
-<header>
-  <div class="back-arrow">
-    <img :src="getImagePath('arrow-back')" href="#shop" v-if="backPossible" @click="goBack">
-  </div>
-
-  <div class="c3-logo">
-    <img :src="getImagePath('c3-logo')" href="#un">
-  </div>
-
-  <div class="right-section">
-    <div class="coins">
-      <img :src="getImagePath('coin')">
-      <p>{{ nbCoins }}</p>
+  <header>
+    <div class="back-arrow">
+      <img
+        :src="getImagePath('arrow-back')"
+        href="#shop"
+        v-if="isBackPossible"
+        @click="goBack"
+      />
     </div>
 
-    <div class="shop">
-      <img :src="getImagePath('shop')" href="#shop">
+    <div class="c3-logo">
+      <img :src="getImagePath('c3-logo')" href="#un" />
     </div>
-  </div>
-</header>
 
+    <div class="right-section">
+      <div class="coins">
+        <img :src="getImagePath('coin')" />
+        <p>{{ nbCoins }}</p>
+      </div>
+
+      <div class="shop">
+        <img :src="getImagePath('shop')" href="#shop" />
+      </div>
+    </div>
+  </header>
 </template>
 
-
-<style>
-    
+<style scoped>
 </style>
