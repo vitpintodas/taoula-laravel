5f5ee39bd539021283fbec65cd4f79f594052c26
Andrea Ferrer <andreaferrer361@gmail.com>
2023-06-08 11:02:08 +0200

    FEAT: pop-up component

--- resources/js/base/BaseAnimateur/BasePopUp.vue
+++ resources/js/base/BaseAnimateur/BasePopUp.vue
@@ -1,16 +1,35 @@
 <script setup>
-import Swal from 'sweetalert2'
 
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+});
+
+const closePopup = () => {
+    emit('update:selected', props.selected)
+};
 
 </script>
 
-
 <template>
-<div class="popup">
+    <div class="popup">
+      <div class="popup-content">
+        <h2>{{ title }}</h2>
+        <p>{{ message }}</p>
+        <button @click="closePopup">Fermer</button>
+      </div>
+    </div>
+  </template>
+  
 
-</div>      
+  
+  <style scoped>
 
-</template>
-
-
-<style></style>
+  </style>
