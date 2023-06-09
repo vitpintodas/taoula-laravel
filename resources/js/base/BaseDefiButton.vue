266d7d1fa243238b0c9e9f7fca3ae22499658f43
Alexandre Aubert <alexandre.aubert@heig.vd.ch>
2023-06-09 11:02:38 +0200

    feat:BaseDefiButton/fix:BaseButton

---
+++ resources/js/base/BaseDefiButton.vue
@@ -0,0 +1,35 @@
+<script setup>
+  import { defineProps } from 'vue'
+  import { getImagePath } from '../utils/getImagePath.js';
+  
+  defineProps({
+    title: {
+      type: String,
+      required: true
+    },
+
+    type: {
+      type: String,
+      required: false
+    },
+
+   
+  })
+</script>
+
+<template>
+  <button class="baseDefiButton-container">
+    
+      <div class="baseDefiButton-type">
+        <img :src="getImagePath(type)"/>
+      </div>
+      <div>
+        <h2 class="titre">{{ title }} </h2>
+      </div>
+    
+  </button>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,27 +1,29 @@
 <script setup>
+import BaseButton from "../base/BaseButton.vue";
 import BaseMainCards from "../base/BaseMainCards.vue";
 </script>
 
 <template>
     <div class="marginT">
-        <BaseMainCards
-            title="Billets pour paléo"
-            description="Tente de ganger des billets pour paléo"
-            type="contest"
-            link="#unlien2"
-        />
-        <BaseMainCards
-            title="Musique star du jour"
-            description="Instant Crush - Daft Punk"
-            type="star"
-            link="#unlien2"
-            info="256 likes"
-        />
-        <BaseMainCards
-            title="Couleur 3 en direct"
-            description="COOL CONTINENTAL"
-            type="live"
-            link="#unlien2"
-        />
+        <BaseMainCards 
+        title="Billets pour paléo" 
+        description="Tente de ganger des billets pour paléo" 
+        type="contest"
+        link="#unlien2" />
+        
+        <BaseMainCards 
+        title="Musique star du jour" 
+        description="Instant Crush - Daft Punk" 
+        type="star" 
+        link="#unlien2"
+        info="256 likes" />
+        
+        <BaseMainCards 
+        title="Couleur 3 en direct" 
+        description="COOL CONTINENTAL" 
+        type="live"
+        link="#unlien2" />
+
+
     </div>
 </template>
