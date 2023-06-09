f59d633110d55191d3e6c1d1ab0251b1739c6b07
Vitor Da Silva <vitor.pintodasilva@heig-vd.ch>
2023-06-09 17:15:09 +0200

    feat (seeders) : ajout role seeders

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = ['admin', 'user'];
+        
+        Role::create([
+            
+        ]);
+    }
+}
