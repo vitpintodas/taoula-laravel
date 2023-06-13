edbd2f9ed4cf494438ebe3f0447fa71b54e424a9
Nathan Gretillat <nathan.gretillat@heig-vd.ch>
2023-06-13 16:47:16 +0200

    STYLE: Page vote, bouton

--- resources/js/base/BaseAnimateur/BaseMultipleInput.vue
+++ resources/js/base/BaseAnimateur/BaseMultipleInput.vue
@@ -1,63 +1,76 @@
 <script setup>
-import { ref } from 'vue';
-import BaseButton from '../../base/BaseButton.vue';
-import BaseInput from '../../base/BaseInput.vue';
+import { ref } from "vue";
+import BaseButton from "../../base/BaseButton.vue";
+import BaseInput from "../../base/BaseInput.vue";
 
 const removeButton = ref(false);
 const addButton = ref(true);
-const extraInputs = ref([{
-    id: 1,
-    value: '',
-}, {
-    id: 2,
-    value: '',
-}]);
+const extraInputs = ref([
+    {
+        id: 1,
+        value: "",
+    },
+    {
+        id: 2,
+        value: "",
+    },
+]);
 
-const addInput = () =>  {
-    if(extraInputs.value.length == 4){
+const addInput = () => {
+    if (extraInputs.value.length == 4) {
         addButton.value = false;
     }
     extraInputs.value.push({
         id: extraInputs.value.length + 1,
-        value: '',
+        value: "",
     });
     removeButton.value = true;
-}
+};
 
 const removeInput = () => {
-    if(extraInputs.value.length > 2){
+    if (extraInputs.value.length > 2) {
         extraInputs.value.pop();
-    } 
+    }
 
-    if(extraInputs.value.length == 2){
+    if (extraInputs.value.length == 2) {
         removeButton.value = false;
     }
     addButton.value = true;
-}
+};
 
-
-//le emit 
-const emits = defineEmits(['emitMultipleInput'])
+//le emit
+const emits = defineEmits(["emitMultipleInput"]);
 const updateInput = (value, input) => {
     //update the input value
     input.value = value;
-    emits('emitMultipleInput', extraInputs.value)
-}
+    emits("emitMultipleInput", extraInputs.value);
+};
 </script>
 
-
 <template>
-    <BaseInput v-for="input in extraInputs" :key="input" :placeholder="`Option/Choix n°${input.id}`" @emit-input="updateInput($event, input)"/>
-    <Button class="add-button" @click="addInput()" v-if="addButton">+ ajouter une option</Button>
-    <Button class="remove-button " v-if="removeButton" @click="removeInput()">- Retirer une option</Button>
+    <BaseInput
+        class="choixPossible-container"
+        v-for="input in extraInputs"
+        :key="input"
+        :placeholder="`Option/Choix n°${input.id}`"
+        @emit-input="updateInput($event, input)"
+    />
+    <Button class="add-button" @click="addInput()" v-if="addButton"
+        >+ Ajouter une option</Button
+    >
+    <Button class="remove-button" v-if="removeButton" @click="removeInput()"
+        >- Retirer une option</Button
+    >
 </template>
 
-
 <style scoped>
-.add-button{
+.choixPossible-container {
+    margin-top: 0px;
+}
+.add-button {
     background: var(--pink);
     color: var(--white);
-    border-radius: 10px;
+    border-radius: 6px;
     justify-content: center;
     align-items: center;
     cursor: pointer;
@@ -68,7 +81,7 @@ const updateInput = (value, input) => {
     padding: 10px;
 }
 
-.add-button:hover{
+.add-button:hover {
     background: rgb(196, 62, 156);
     color: var(--white);
     border-radius: 10px;
@@ -80,7 +93,7 @@ const updateInput = (value, input) => {
     max-width: fit-content;
 }
 
-.remove-button{
+.remove-button {
     background: rgb(222, 80, 80);
     color: var(--white);
     border-radius: 10px;
@@ -93,7 +106,7 @@ const updateInput = (value, input) => {
     padding: 10px;
 }
 
-.remove-button:hover{
+.remove-button:hover {
     background: rgb(197, 57, 57);
     color: var(--white);
     border-radius: 10px;
--- resources/js/components/Animateur/Vote.vue
+++ resources/js/components/Animateur/Vote.vue
@@ -1,57 +1,58 @@
 <script setup>
-import { ref } from 'vue';
-import BaseButton from '../../base/BaseButton.vue';
-import BaseInput from '../../base/BaseInput.vue';
-import BaseMultipleInput from '../../base/BaseAnimateur/BaseMultipleInput.vue';
-import BaseDurationInput from '../../base/BaseAnimateur/BaseDurationInput.vue';
+import { ref } from "vue";
+import BaseButton from "../../base/BaseButton.vue";
+import BaseInput from "../../base/BaseInput.vue";
+import BaseMultipleInput from "../../base/BaseAnimateur/BaseMultipleInput.vue";
+import BaseDurationInput from "../../base/BaseAnimateur/BaseDurationInput.vue";
 
 const vote = ref({
-    title: '',
+    title: "",
     choices: [],
-    time: '',
+    time: "",
 });
 
 const updateChoices = (val) => {
     vote.value.choices = val;
-}
+};
 
 const updateTime = (timeVal) => {
-    console.log('TIME !', timeVal);
+    console.log("TIME !", timeVal);
     vote.value.time = timeVal;
-}
+};
 
 const launchVote = () => {
-    if(vote.value.title == '' || vote.value.choices.length < 2 || vote.value.time == ''){
-        alert('Veuillez entrer un titre pour le vote');
+    if (
+        vote.value.title == "" ||
+        vote.value.choices.length < 2 ||
+        vote.value.time == ""
+    ) {
+        alert("Veuillez entrer un titre pour le vote");
         return;
     }
-    console.log('Final', vote.value);
-
-}
+    console.log("Final", vote.value);
+};
 </script>
-
 
 <template>
     <h1 class="titre2">Sujet du vote</h1>
-    <BaseInput placeholder="Sujet du vote" @emit-input="vote.title = $event"/>
+    <BaseInput placeholder="Sujet du vote" @emit-input="vote.title = $event" />
     <h1 class="titre2">Choix possibles</h1>
 
-    <BaseMultipleInput @emit-multiple-input="updateChoices($event)"/>
+    <BaseMultipleInput @emit-multiple-input="updateChoices($event)" />
 
-    <BaseDurationInput @emit-time="updateTime($event)"/>
+    <BaseDurationInput @emit-time="updateTime($event)" />
 
-    <BaseButton class="vote-button" title="Lancer le vote" size="large"  @click="launchVote()"/>
-
+    <BaseButton
+        title="Lancer le vote"
+        size="large"
+        @click="launchVote()"
+    />
 </template>
 
-
 <style scoped>
-@media screen and (max-width: 1206px){
-    a.vote-button {
-    float: left;
-    margin-left: calc(30% + 50px);
-    width: 450px;
-}
-}
 
+.baseInput-container {
+    padding-top: 0px;
+    margin-top: 0px;
+}
 </style>
