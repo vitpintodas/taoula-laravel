32074b2ed984a356344559786884df708cade90c
vitorema-pintodas <vitor.pintodasilva@heig-vd.ch>
2023-06-13 22:15:44 +0200

    feat (api) : ajout api titres + artistes

---
+++ app/Http/Controllers/ArtisteController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Artiste;
+use Illuminate\Http\Request;
+
+class ArtisteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $artiste = Artiste::findOrFail($id);
+        return response()->json($artiste);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
