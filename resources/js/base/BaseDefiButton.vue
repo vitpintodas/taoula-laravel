7bcbf21e65406498a49f1efeb14cf666dbb9cd82
Nathan Gretillat <nathan.gretillat@heig-vd.ch>
2023-06-13 17:56:27 +0200

    FEAT: navigation dans les défis

--- resources/js/base/BaseDefiButton.vue
+++ resources/js/base/BaseDefiButton.vue
@@ -1,38 +1,61 @@
-<script setup>
-import { defineProps, ref } from "vue";
-  import { getImagePath } from '../utils/getImagePath.js';
-
+<template>
+    <div class="baseDefiButton-container couleurActiveButton">
+      <ul>
+        <li>
+          <a :class="{ active: isActive }" @click="setActive">
+            <img :src="getImagePath(type)" class="baseDefiButton-type" />
+            <h2 class="titre">{{ title }}</h2>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps, ref, onMounted } from "vue";
+  import { getImagePath } from "../utils/getImagePath.js";
   
   const props = defineProps({
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     type: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+    },
+  });
+  
+  const isActive = ref(false);
+  
+  const setActive = () => {
+    isActive.value = true;
+    const otherButtons = document.querySelectorAll(".couleurActiveButton a.active");
+    otherButtons.forEach((button) => {
+      button.classList.remove("active");
+    });
+  
+    // Mettre à jour l'URL avec le paramètre d'état "actif"
+    const url = new URL(window.location.href);
+    url.searchParams.set("activeButton", props.type);
+    window.history.replaceState({}, "", url);
+  };
+  
+  onMounted(() => {
+    // Vérifier le paramètre d'état "actif" dans l'URL lors du chargement de la page
+    const url = new URL(window.location.href);
+    const activeButton = url.searchParams.get("activeButton");
+    if (activeButton === props.type) {
+      isActive.value = true;
     }
-});
-
-const isActive = ref(false);
-
-</script>
-
-
-<template>
-    <div class="baseDefiButton-container">
-        <ul>
-            <li>
-                <a :class="{ active: isActive }">
-                  <img :src="getImagePath(type)" class="baseDefiButton-type"/>
-                  <h2 class="titre">{{ title }}  </h2>
-                </a>
-            </li>
-        </ul>
-    </div>
-</template>
-
-
+  });
+  </script>
+  
+  <style scoped>
+  .active {
+    background-color: var(--pink);
+  }
+  </style>
+  
 
 <!-- css : baseDefiButton.css -->
-
--- resources/js/components/Animateur/Defi.vue
+++ resources/js/components/Animateur/Defi.vue
@@ -1,9 +1,40 @@
-<script setup>
-import BaseDefiButton from '../../base/BaseDefiButton.vue';
-import BaseInput from '../../base/BaseInput.vue';
-import { ref } from "vue";
-import isActive from '../../base/BaseDefiButton.vue';
-
+<template>
+    <div class="defi-container">
+      <h2>Sujet du défi</h2>
+      <BaseInput class="defi-input" placeholder="Sujet du défi..." @input="changeValue($event)">
+      </BaseInput>
+      <h2>Type du défi</h2>
+      <div class="typeDefi-container">
+        <div>
+          <BaseDefiButton title="Rapidité" type="fast" :class="{ active: defiRapidite }" @click="rapidite"></BaseDefiButton>
+          <BaseDefiButton title="Tirage parmi bonne réponse" type="box" :class="{ active: defiTirage }" @click="tirage"></BaseDefiButton>
+        </div>
+        <div class="defi2">
+          <BaseDefiButton title="Gagnant sélectionné à la main" type="thumbsUp" :class="{ active: defiSelection }" @click="gagne"></BaseDefiButton>
+          <BaseDefiButton title="Les bonnes réponses gagnent" type="thumbsUp" :class="{ active: defiMain }" @click="main"></BaseDefiButton>
+        </div>
+      </div>
+      <div class="affiche-defi">
+        <div v-if="defiRapidite" id="defi-rapidite">
+          <h1>caca1</h1>
+        </div>
+        <div v-if="defiTirage" id="defi-tirage">
+          <h1>caca2</h1>
+        </div>
+        <div v-if="defiSelection" id="defi-main">
+          <h1>caca3</h1>
+        </div>
+        <div v-if="defiMain" id="defi-gagne">
+          <h1>caca4</h1>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+import { ref, onMounted } from "vue";
+import BaseDefiButton from "../../base/BaseDefiButton.vue";
+import BaseInput from "../../base/BaseInput.vue";
 
 const defiRapidite = ref(false);
 const defiTirage = ref(false);
@@ -11,72 +42,66 @@ const defiSelection = ref(false);
 const defiMain = ref(false);
 
 const rapidite = () => {
-    defiRapidite.value = true;
-    defiTirage.value = false;
-    defiSelection.value = false;
-    defiMain.value = false;
-    isActive.value = true;
-}
+  defiRapidite.value = true;
+  defiTirage.value = false;
+  defiSelection.value = false;
+  defiMain.value = false;
+  updateActiveState("rapidite");
+};
 
 const tirage = () => {
-    defiRapidite.value = false;
-    defiTirage.value = true;
-    defiSelection.value = false;
-    defiMain.value = false;
-    isActive.value = true;
-}
+  defiRapidite.value = false;
+  defiTirage.value = true;
+  defiSelection.value = false;
+  defiMain.value = false;
+  updateActiveState("tirage");
+};
 
 const gagne = () => {
-    defiRapidite.value = false;
-    defiTirage.value = false;
-    defiSelection.value = true;
-    defiMain.value = false;
-    isActive.value = true;
-}
+  defiRapidite.value = false;
+  defiTirage.value = false;
+  defiSelection.value = true;
+  defiMain.value = false;
+  updateActiveState("selection");
+};
 
 const main = () => {
-    defiRapidite.value = false;
-    defiTirage.value = false;
-    defiSelection.value = false;
-    defiMain.value = true;
-    isActive.value = true;
-}
+  defiRapidite.value = false;
+  defiTirage.value = false;
+  defiSelection.value = false;
+  defiMain.value = true;
+  updateActiveState("main");
+};
 
+const updateActiveState = (activeButton) => {
+  const url = new URL(window.location.href);
+  url.searchParams.set("activeButton", activeButton);
+  window.history.replaceState({}, "", url);
+};
+
+onMounted(() => {
+  const url = new URL(window.location.href);
+  const activeButton = url.searchParams.get("activeButton");
+  if (activeButton) {
+    switch (activeButton) {
+      case "rapidite":
+        rapidite();
+        break;
+      case "tirage":
+        tirage();
+        break;
+      case "selection":
+        gagne();
+        break;
+      case "main":
+        main();
+        break;
+      default:
+        break;
+    }
+  }
+});
 </script>
-
-<template>
-    <div class="defi-container">
-        <h2>Sujet du défi</h2>
-        <BaseInput class="defi-input" placeholder="Sujet du défi..." @input="changeValue($event)">
-        </BaseInput>
-        <h2>Type du défi</h2>
-        <div class="typeDefi-container">
-            <div>
-                <BaseDefiButton title="Rapidité" type="fast" @click="rapidite()" ></BaseDefiButton>
-                <BaseDefiButton title="Tirage parmi bonne réponse" type="box" @click="tirage()"></BaseDefiButton>
-            </div>
-            <div class="defi2">
-                <BaseDefiButton title="Gagnant sélectionné à la main" type="thumbsUp"  @click="main()">
-                </BaseDefiButton>
-                <BaseDefiButton title="Les bonnes réponses gagnent" type="thumbsUp" @click="gagne()"></BaseDefiButton>
-            </div>
-
-        </div>
-        <div class="affiche-defi">
-            <div v-if="defiRapidite" id="defi-rapidite">
-                <h1>caca1</h1>
-            </div>
-            <div v-if="defiTirage" id="defi-tirage">
-                <h1>caca2</h1>
-            </div>
-            <div v-if="defiSelection" id="defi-main">
-                <h1>caca3</h1>
-            </div>
-            <div v-if="defiMain" id="defi-gagne">
-                <h1>caca4</h1>
-            </div>
-        </div>
-    </div>
-</template>
-
-<style></style>
+  
+  <style></style>
+  
