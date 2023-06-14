dda71406e40c3e12e5b5ab564b8a3409cb8c1396
Alexandre Aubert <alexandre.aubert@heig.vd.ch>
2023-06-14 12:37:30 +0200

    fix:MusicChoice

--- resources/js/components/Animateur/MusicChoice.vue
+++ resources/js/components/Animateur/MusicChoice.vue
@@ -1,30 +1,55 @@
 <script setup>
 import BaseInput from '../../base/BaseInput.vue';
 import BaseButton from '../../base/BaseButton.vue';
+import BaseMultipleInput from "../../base/BaseAnimateur/BaseMultipleInput.vue";
+import { ref } from "vue";
+
+const choice = ref({
+    choices: [],
+});
+
+const updateChoices = (val) => {
+    choice.value.choices = val;
+};
+
+
+
+const launchChoice = () => {
+    if (
+        choice.value.choices.length < 2
+    ) {
+        alert("Veuillez entrer un titre pour le vote");
+        return;
+    }
+    console.log("Final", choice.value);
+
+};
 
 </script>
 
 
 <template>
-<!-- Titre de la page -->
-<h1 class="titre2">Choix possibles</h1>
+    <!-- Titre de la page -->
+    <h1 class="titre2">Choix possibles</h1>
+    <div class="multipleChoice">
 
-<!-- Premier input permettant de proposer une musique -->
+    <BaseMultipleInput @emit-multiple-input="updateChoices($event)" />
+</div>
+    <!-- Premier input permettant de proposer une musique 
 <BaseInput placeholder="Choix numéro 1" class="concours-basInput"/>
-<!-- Deuxième input permettant de proposer une musique -->
+ Deuxième input permettant de proposer une musique 
 <BaseInput placeholder="Choix numéro 2" class="concours-basInput"/>
-<!-- Troisième input permettant de proposer une musique -->
-<BaseInput placeholder="Choix numéro 3" class="concours-basInput"/>
+Troisième input permettant de proposer une musique 
+<BaseInput placeholder="Choix numéro 3" class="concours-basInput"/> -->
 
-<!-- Bouton permmettant d'envoyer sur l'interface utilisateur les musiques choisies au-dessus -->
-<BaseButton title="Lancer le vote" 
-            size="large" 
-            link="#musicChoices"
-/>
-
+    <!-- Bouton permmettant d'envoyer sur l'interface utilisateur les musiques choisies au-dessus -->
+    <BaseButton title="Lancer le vote" size="large" link="#musicChoices" @click="launchChoice()" />
 </template>
 
 
 <style>
-
+.multipleChoice {
+    padding-bottom: 25px;
+  
+}
 </style>
