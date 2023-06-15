c4e50934c66ccd07ecb03e768898a4a66f29cbd7
Nathan Gretillat <nathan.gretillat@heig-vd.ch>
2023-06-15 17:15:20 +0200

    FEAT: Page search avec api, recherche fix et rediffusions fonctionnelles

--- resources/js/components/Search.vue
+++ resources/js/components/Search.vue
@@ -1,57 +1,166 @@
+<template>
+    <div>
+        <BaseInput
+            class="marginTB marginLR"
+            :value="filtre"
+            type="searchInput"
+            placeholder="Rechercher..."
+            @emit-input="updateFiltre($event)"
+        />
+        <p class="titre2 marginLR marginT marginB" v-if="showTitle">
+            Rediffusions de la semaine
+        </p>
+
+        <div
+            class="marginLR marginT cardSolo"
+            v-for="(cards, date) in filteredCards"
+            :key="date"
+        >
+            <p class="small-description" v-if="showTitle && cards.length > 0">
+                {{ date }}
+            </p>
+            <BaseSmallCards
+                v-for="card in cards"
+                :key="card.title"
+                :title="card.title"
+                :type="card.type"
+                :likeStatus="card.likeStatus"
+            />
+        </div>
+    </div>
+</template>
+
 <script setup>
-import { ref, watch } from 'vue';
+import { ref, watch } from "vue";
 import BaseInput from "../base/BaseInput.vue";
 import BaseSmallCards from "../base/BaseSmallCards.vue";
+import { getArtists, getTitles } from "../utils/loadJson";
 
-const cards = ref([
-  {
-    title: "ACDC - Al in blak",
-    type: "music",
-    likeStatus: false,
-  },
-  {
-    title: "ACDC - All in black",
-    type: "music",
-    likeStatus: false,
-  },
-  // ... Ajoutez les autres cartes ici
-]);
+const cards = ref([]);
 
-const filtre = ref('');
+for (let i = 0; i < 15; i++) {
+    const currentDate = getPastDate(i);
+    cards.value.push({
+        title: `Card ${i + 1}`,
+        type: "music",
+        likeStatus: false,
+        date: currentDate,
+    });
+}
+
+const artists = ref([]);
+const titles = ref([]);
+
+const fetchData = async () => {
+    for (let index = 0; index < 15; index++) {
+        const artistData = await getArtists(index + 1);
+        artists.value[index] = artistData;
+
+        const titleData = await getTitles(index + 1);
+        titles.value[index] = titleData;
+
+        cards.value[
+            index
+        ].title = `${artists.value[index].nom} - ${titles.value[index].nom}`;
+    }
+
+    updateFiltre(filtre.value);
+};
+
+fetchData();
+
+const filtre = ref("");
 const showTitle = ref(true);
 
 const updateFiltre = (val) => {
-  filtre.value = val;
-  showTitle.value = val === ''; // Vérifie si la valeur du filtre est vide
+    filtre.value = val;
+    showTitle.value = val === "";
 };
 
-const filteredCards = ref(cards.value);
+const filteredCards = ref(groupCardsByDate(cards.value, 5, 5));
 
 watch(filtre, (val) => {
-  if (val === "") {
-    filteredCards.value = cards.value;
-  } else {
-    filteredCards.value = cards.value.filter((card) =>
-      card.title.toLowerCase().includes(val.toLowerCase())
-    );
-  }
+    if (val === "") {
+        filteredCards.value = groupCardsByDate(cards.value, 5, 5);
+    } else {
+        filteredCards.value = groupCardsByDate(
+            cards.value.filter((card) =>
+                card.title.toLowerCase().includes(val.toLowerCase())
+            ),
+            Infinity,
+            Infinity
+        );
+    }
 });
+
+// Fonction pour regrouper les cartes par date avec 1 à 2 cartes par date (ou toutes les cartes si maxCardsPerDate = Infinity)
+function groupCardsByDate(cards, maxDates, maxCardsPerDate) {
+    const groupedCards = {};
+
+    const uniqueDates = [...new Set(cards.map((card) => card.date))];
+    const sortedDates = uniqueDates.sort((a, b) => new Date(b) - new Date(a));
+
+    let dateCount = 0;
+
+    for (const date of sortedDates) {
+        const dateCards = cards.filter((card) => card.date === date);
+        if (dateCards.length > 0) {
+            groupedCards[date] = getRandomCards(dateCards, maxCardsPerDate);
+            dateCount++;
+        }
+
+        if (dateCount >= maxDates) {
+            break;
+        }
+    }
+
+    return groupedCards;
+}
+
+// Fonction pour obtenir 1 à 2 cartes aléatoires d'un tableau de cartes
+function getRandomCards(cards, maxCards) {
+    if (cards.length <= maxCards || maxCards === Infinity) {
+        return cards;
+    } else {
+        const randomIndices = new Set();
+        while (randomIndices.size < maxCards) {
+            const randomIndex = Math.floor(Math.random() * cards.length);
+            randomIndices.add(randomIndex);
+        }
+        return Array.from(randomIndices).map((index) => cards[index]);
+    }
+}
+
+// Fonction pour obtenir la date d'aujourd'hui au format jj.mm.aaaa
+function getCurrentDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
+    return `${day}.${month}.${year}`;
+}
+
+// Fonction pour obtenir la date passée en fonction du nombre de jours écoulés
+function getPastDate(days) {
+    const today = new Date();
+    const pastDate = new Date(today.setDate(today.getDate() - days));
+    const year = pastDate.getFullYear();
+    const month = String(pastDate.getMonth() + 1).padStart(2, "0");
+    const day = String(pastDate.getDate()).padStart(2, "0");
+    return `${day}.${month}.${year}`;
+}
 </script>
 
-<template>
-  <BaseInput class="marginTB marginLR" :value="filtre" type="searchInput" placeholder="Rechercher..." @emit-input="updateFiltre($event)" />
-  <p class="titre2 marginLR marginT marginB" v-if="showTitle">Rediffusions de la semaine</p>
-
-  <div class="marginLR">
-    <p class="small-description" v-if="showTitle">14.06.2023</p>
-    <BaseSmallCards v-for="card in filteredCards" :key="card.title" :title="card.title" :type="card.type" :likeStatus="card.likeStatus" />
-  </div>
-  <div class="marginLR">
-    <p class="small-description marginT" v-if="showTitle">05.06.2023</p>
-    <BaseSmallCards v-for="card in filteredCards" :key="card.title" :title="card.title" :type="card.type" :likeStatus="card.likeStatus" />
-  </div>
-
-</template>
-
 <style scoped>
+.titre2 {
+    margin-bottom: -20px;
+}
+
+.cardSolo {
+    margin-bottom: -10px;
+}
+
+.small-description {
+    padding-top: 10px;
+}
 </style>
--- resources/js/utils/loadJson.js
+++ resources/js/utils/loadJson.js
@@ -1,48 +1,31 @@
 const BASE_URL = "http://localhost:8000/api/";
 
 function loadJson(url) {
-    return fetch(url, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({}),
-    })
-        .then((response) => response.json())
-        .then((data) => console.log("Success:", data))
-        .catch((error) => {
-            console.error("Error:", error);
-        });
-}
-function getArtists(nb) {
-    return loadJson(`${BASE_URL}artistes/${nb}`);
-}
+    return fetch(url).then((response) => response.json())
+  }
 
-function getUser(nb) {
-    return loadJson(`${BASE_URL}users/${nb}`);
-}
+  function getArtists(nb) {
+    return loadJson(`${BASE_URL}artistes/${nb}`)   
+  }
 
-function getTitles(nb) {
-    return loadJson(`${BASE_URL}titres/${nb}`);
-}
+  function getUser(nb) {
+    return loadJson(`${BASE_URL}users/${nb}`)
+  }
 
-function getPlaylists(nb) {
-    return loadJson(`${BASE_URL}playlists/${nb}`);
-}
+  function getTitles(nb) {
+    return loadJson(`${BASE_URL}titres/${nb}`)
+  }
+
+  function getPlaylists(nb) {
+    return loadJson(`${BASE_URL}playlists/${nb}`)
+  }
 
 function getResponse(nb) {
-    return loadJson(`${BASE_URL}reponses/${nb}`);
+    return loadJson(`${BASE_URL}reponses/${nb}`)
 }
 
 function getSondages(nb) {
-    return loadJson(`${BASE_URL}sondages/${nb}`);
+    return loadJson(`${BASE_URL}sondages/${nb}`)
 }
 
-export {
-    getArtists,
-    getUser,
-    getTitles,
-    getPlaylists,
-    getResponse,
-    getSondages,
-};
+export { getArtists, getUser, getTitles, getPlaylists, getResponse, getSondages }
